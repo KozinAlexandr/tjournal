2cd26be348905216b7fe6ca5205643dcade4f911
Alexandr Kozin <sasha.kozin.2014alex444@ya.ru>
2022-03-25 00:34:01 +0300

    add skilet site(search,pen,message,notif)

---
+++ components/Header/index.tsx
@@ -0,0 +1,45 @@
+import { Paper, Button, IconButton, Avatar } from '@material-ui/core';
+import React from 'react';
+import {
+  SearchOutlined as SearchIcon, 
+  CreateOutlined as PenIcon, 
+  SmsOutlined as MessageIcon,
+  NotificationsOutlined as NotificationIcon,
+} from '@mui/icons-material/';
+
+import styles from './Header.module.scss';
+
+export const Header: React.FC = () => {
+  return (
+    <Paper classes={{root: styles.root}} elevation={0}>
+      <div>
+        <svg className={styles.logo} viewBox="0 0 24 25">
+          <path fill = "#e8a427" d="M0 19h8.5v6H0v-6z"></path>
+          <path d="M0 7h8.5v18l6.5-6V7h9V0H0V7z"></path>
+          <path fill = "rgba(0,0,0,0.15)" d="M7.5 19h1v6l-1-6z"></path>
+        </svg>
+        
+        <div className={styles.searchBlock}>
+          <SearchIcon/>
+          <input placeholder="Поиск"/>
+        </div>
+        
+        <Button>
+          <PenIcon/>
+        </Button>
+      </div>
+
+      <div>
+        <IconButton>
+          <MessageIcon/>
+        </IconButton>
+        <IconButton>
+          <NotificationIcon/>
+        </IconButton>
+        <Avatar alt="Alexandr Kozin" src="https://avatars.githubusercontent.com/u/66381294?v=4" />
+      </div>
+
+    </Paper>
+  )
+}
+//React.FC - функциональный компонент
