0d5def360f27e91cd6fa61c7e440e3baaf4bb181
Alexandr Kozin <sasha.kozin.2014alex444@ya.ru>
2022-03-30 01:03:06 +0300

    add EditorJs and page write

---
+++ components/Editor.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import EditorJS from '@editorjs/editorjs';
+
+export const Editor: React.FC=()=>{
+  React.useEffect(()=>{
+    const editor = new EditorJS({
+      holder: 'editor'
+    });
+    return()=> {
+      editor.isReady.then(()=>{
+        editor.destroy();
+      })
+      .catch(e => console.error('ERROR editor cleanup', e));
+    }
+  },[]);
+  return(
+    <div id = "editor" />
+  );
+};
---
+++ components/WriteForm/index.tsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import styles from './WriteForm.module.scss';
+import { Input } from '@material-ui/core';
+import dynamic from 'next/dynamic';
+
+const Editor = dynamic(()=> import('../Editor').then(m=>m.Editor), {ssr:false})
+//ne na servere render a na kliente, dynamic podgruzka
+
+interface WriteFormProps{
+  title?: string;
+}
+
+export const WriteForm: React.FC<WriteFormProps> = ({title})=>{
+  return(
+    <div>
+      <Input classes={{ root: styles.titleField,}} placeholder="Заголовок" defaultValue={title}/>
+      <Editor/>
+    </div>
+  );
+};
