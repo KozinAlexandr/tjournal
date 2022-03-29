d0ab95e3dc5bdc77936be0acb20028662974aa3b
Alexandr Kozin <sasha.kozin.2014alex444@ya.ru>
2022-03-30 01:22:42 +0300

    add button in write page

--- components/WriteForm/index.tsx
+++ components/WriteForm/index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import styles from './WriteForm.module.scss';
-import { Input } from '@material-ui/core';
+import { Button, Input } from '@material-ui/core';
 import dynamic from 'next/dynamic';
 
 const Editor = dynamic(()=> import('../Editor').then(m=>m.Editor), {ssr:false})
@@ -15,7 +15,12 @@ export const WriteForm: React.FC<WriteFormProps> = ({title})=>{
   return(
     <div>
       <Input classes={{ root: styles.titleField,}} placeholder="Заголовок" defaultValue={title}/>
-      <Editor/>
+      <div className={styles.editor}>
+        <Editor/>
+      </div>
+      <Button variant="contained" color="primary">
+          Опубликовать
+      </Button>
     </div>
   );
 };
